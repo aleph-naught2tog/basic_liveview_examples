52cf16daa888df63a8a51dbcc3d26eea99f04ee4
Max Cerrina <mshemayev@gmail.com>
2023-08-24 09:49:02 -0500

    Add example Phoenix Component

--- lib/basic_liveview_web/components/zoo.ex
+++ lib/basic_liveview_web/components/zoo.ex
@@ -1,21 +1,38 @@
 defmodule BasicLiveviewWeb.Zoo do
   use Phoenix.Component
 
-  def animal_action_buttons(assigns) do
+  attr(:name, :string,
+    doc: "The name of the animal. This should be singular.",
+    examples: ["dog", "cat", "horse", "pterodactyl"]
+  )
+
+  attr(:count, :integer, doc: "The current count of that animal.")
+
+  attr(:add_event, :string,
+    doc: "The name of the event for adding that animal.",
+    examples: ["add-parrot", "obtain-lemming"]
+  )
+
+  attr(:remove_event, :string,
+    doc:
+      "The name of the event for remove that animal. Remember, this must match your `handle_event` arguments.",
+    examples: ["take-iguana-to-groomers", "take-kitten-to-cat-cafe", "adopt-out-guinea-pig"]
+  )
+
+  def animal_count(assigns) do
     ~H"""
-    <div class="actions">
-      <div class="button-wrapper">
-        <%!-- The value of 'phx-click' needs to exactly match the first argument of `handle_event` --%>
-        <button phx-click="add-cat">Add cat</button>
-        <button phx-click="remove-cat" disabled={@zoo_map[:cats] === 0}>
-          Give cat vacation
-        </button>
-      </div>
+    <div>
+      <dl class="animal-list">
+        <dt>
+          <span role="img" aria-label={@name}>🐈</span> <%= @name <> "s" %>
+        </dt>
+        <dd><%= @count %></dd>
+      </dl>
 
       <div class="button-wrapper">
-        <button phx-click="add-dog">Add dog</button>
-        <button phx-click="remove-dog" disabled={@zoo_map[:dogs] === 0}>
-          Give dog vacation
+        <button phx-click={@add_event}>Add <%= @name %></button>
+        <button phx-click={@remove_event} disabled={@count === 0}>
+          Give <%= @name %> vacation
         </button>
       </div>
     </div>
